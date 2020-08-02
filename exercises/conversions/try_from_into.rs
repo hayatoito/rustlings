bad905ec6c13dd028f9f6f7ef34ffb2935e62041
Hayato Ito <hayatoito@gmail.com>
2020-08-02 19:06:47 +0900

    done

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -11,8 +11,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need create implementation for a tuple of three integer,
@@ -26,6 +24,16 @@ struct Color {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        match tuple {
+            (0..=255, 0..=255, 0..=255) => {
+                Ok(Color {
+                    red: tuple.0 as u8,
+                    green: tuple.1 as u8,
+                    blue: tuple.2 as u8,
+                })
+            }
+            _ => Err("out of range".to_string())
+        }
     }
 }
 
@@ -33,6 +41,7 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        (arr[0], arr[1], arr[2]).try_into()
     }
 }
 
@@ -40,6 +49,11 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = String;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() == 3 {
+            (slice[0], slice[1], slice[2]).try_into()
+        } else {
+            Err("slice len is invalid".to_string())
+        }
     }
 }
 
